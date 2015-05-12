1b74739eb95212ac20d8cb1440dcccf5ef15d949
Raptis Dimos <raptis.dimos@yahoo.gr>
2015-05-12 22:53:34 +0300

    improved buttons interaction

---
+++ js/ui.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+
+    $('.colors').click(function(){
+		//$(this).animate({"width": "-=5px", "height": "-=5px" }, "fast" ).animate({"width": "+=5px", "height": "+=5px" }, "fast" );
+		$(this).animate({"width": "-=8px"}, "fast" ).animate({"width": "+=8px"}, "fast" );
+	});
+
+});
